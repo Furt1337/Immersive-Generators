5caaba58d57bae7a79d581f1d4421653c8b9270a
Furt <furt1337@live.com>
2016-09-18 04:10:43 -0500

    working on multiblock functions

--- src/main/java/me/furt/immersivegenerators/blocks/multiblocks/MultiblockCoalGenerator.java
+++ src/main/java/me/furt/immersivegenerators/blocks/multiblocks/MultiblockCoalGenerator.java
@@ -1,16 +1,25 @@
 package me.furt.immersivegenerators.blocks.multiblocks;
 
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.block.model.ItemCameraTransforms;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import blusunrize.immersiveengineering.api.MultiblockHandler.IMultiblock;
 import blusunrize.immersiveengineering.api.crafting.IngredientStack;
+import blusunrize.immersiveengineering.client.ClientUtils;
+import blusunrize.immersiveengineering.common.IEContent;
+import blusunrize.immersiveengineering.common.blocks.metal.BlockTypes_MetalDecoration0;
+import blusunrize.immersiveengineering.common.blocks.metal.BlockTypes_MetalMultiblock;
 
 public class MultiblockCoalGenerator implements IMultiblock {
-	private static int height = 6,length = 5,width = 4;
+	private static int height = 6,length = 7,width = 4;
 
 	public static MultiblockCoalGenerator instance = new MultiblockCoalGenerator();
 	static ItemStack[][][] structure = new ItemStack[height][length][width];
@@ -25,8 +34,7 @@ public class MultiblockCoalGenerator implements IMultiblock {
 	
 	@Override
 	public boolean canRenderFormedStructure() {
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	@Override
@@ -44,20 +52,24 @@ public class MultiblockCoalGenerator implements IMultiblock {
 
 	@Override
 	public ItemStack[][][] getStructureManual() {
-		// TODO Auto-generated method stub
-		return null;
+		return structure;
 	}
 
+	static final IngredientStack[] materials = new IngredientStack[]{
+		new IngredientStack("scaffoldingSteel", 24),
+		new IngredientStack(new ItemStack(IEContent.blockMetalDecoration0, 1, BlockTypes_MetalDecoration0.RS_ENGINEERING.getMeta())),
+		new IngredientStack(new ItemStack(IEContent.blockMetalDecoration0, 8, BlockTypes_MetalDecoration0.GENERATOR.getMeta())),
+		new IngredientStack(new ItemStack(IEContent.blockMetalDecoration0, 23, BlockTypes_MetalDecoration0.LIGHT_ENGINEERING.getMeta())),
+		new IngredientStack(new ItemStack(IEContent.blockMetalDecoration0, 5, BlockTypes_MetalDecoration0.RADIATOR.getMeta()))};
+	
 	@Override
 	public IngredientStack[] getTotalMaterials() {
-		// TODO Auto-generated method stub
-		return null;
+		return materials;
 	}
 
 	@Override
 	public String getUniqueName() {
-		// TODO Auto-generated method stub
-		return null;
+		return "IG:CoalGenerator";
 	}
 
 	@Override
@@ -67,15 +79,27 @@ public class MultiblockCoalGenerator implements IMultiblock {
 	}
 
 	@Override
+	@SideOnly(Side.CLIENT)
 	public boolean overwriteBlockRender(ItemStack arg0, int arg1) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
+	@SideOnly(Side.CLIENT)
+	static ItemStack renderStack;
+	
 	@Override
+	@SideOnly(Side.CLIENT)
 	public void renderFormedStructure() {
-		// TODO Auto-generated method stub
-		
+		if(renderStack==null)
+			 renderStack = new ItemStack(IEContent.blockMetalMultiblock,1,1);
+		GlStateManager.scale(-5.5, 5.5, 5.5);
+		GlStateManager.translate(0, .09375, 0);
+		GlStateManager.rotate(225, 0, 1, 0);
+		GlStateManager.rotate(-20, 1, 0, 0);
+
+		GlStateManager.disableCull();
+		ClientUtils.mc().getRenderItem().renderItem(renderStack, ItemCameraTransforms.TransformType.GUI);
+		GlStateManager.enableCull();
 	}
 
 }
